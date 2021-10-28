2a067b173bf7a00f2621bfe6f9077bdade5219cc
Bionic_Scout <roman.higginson@zoominternet.net>
2021-10-28 09:58:11 -0400

    Script stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,14 +6,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
-    public float restartDelay = 1f;
     public GameObject completeLevelUI;
 
     public void CompleteLevel()
     {
         completeLevelUI.SetActive(true);
     }
-    public void EndGame ()
+    public void EndGame (float restartDelay)
     {
         if(gameHasEnded == false)
         {
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour
     //Player
     public GameObject player;
     public Rigidbody rb;
-    Vector3 startPos;
 
     //Speed
     float Speed = 25f;
@@ -25,10 +24,8 @@ public class PlayerMovement : MonoBehaviour
     //Reset List
     List<GameObject> moved = new List<GameObject>();
 
-    private void Start()
-    {
-        startPos = player.transform.position;
-    }
+    //Delay
+    float delay = 1.5f;
 
     private void Update()
     {
@@ -36,15 +33,15 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetButtonUp("Restart"))
         {
-            restart();
+            restart(0);
             Debug.Log("Restart");
         }
 
-        //if (player.transform.position.y < -5)
-        //{
-        //    restart();
-        //    Debug.Log("Fell Off");
-        //}
+        if (player.transform.position.y < -5)
+        {
+           restart(delay);
+           Debug.Log("Fell Off");
+        }
     }
 
     // Update is called once per frame
@@ -111,12 +108,8 @@ public class PlayerMovement : MonoBehaviour
         {
             if (hasShield) //If Player Has Shiled they keep going
             {
-                //Move obsticle below scene
-               // Vector3 temp = collisionInfo.gameObject.transform.position;
-                //collisionInfo.gameObject.transform.position = new Vector3(temp.x, temp.y - 5, temp.z);
-                Debug.Log("Protected");
 
-               // moved.Add(collisionInfo.gameObject);
+                Debug.Log("Protected");
 
                 //Deal with Shield
                 Destroy(shield);
@@ -127,7 +120,7 @@ public class PlayerMovement : MonoBehaviour
                 enabled = false;
                 Debug.Log("Dead");
 
-                restart();
+                restart(delay);
             }
         }
 
@@ -138,26 +131,8 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void restart()
+    void restart(float restartDelay)
     {
-
-        player.transform.position = startPos;
-        player.transform.Rotate(0.0f, 0.0f, 0.0f, Space.Self);
-
-        Debug.Log("Restart");
-        for (int i = 0; i < moved.Count; i++)
-        {
-            Vector3 temp = moved[i].transform.position;
-            moved[i].transform.position = new Vector3(temp.x, temp.y + 5, temp.z);
-            Debug.Log("Returned");
-        }
-
-        Destroy(shield);
-        hasShield = false;
-
-        moved.Clear();
-
-        rb.velocity = Vector3.zero;
-        enabled = true;
+        FindObjectOfType<GameManager>().EndGame(restartDelay);
     }
 }
